af2be3b49e5088e64b569b7ea5ab84655e310b51
A2525-sketch <aadams16@uoguelph.ca>
2026-03-01 18:42:12 -0500

    edited readme (further changes to be taken on by another group member) removed some code from MainApp

---
+++ src/README.md
@@ -0,0 +1,7 @@
+Run the program from MainApp to have access to all options.
+
+Options include: Event Manager, Waitlist Manager, Booking Manager, User Manager
+
+New window will display information based on option selected
+
+To end program simply close all windows
--- src/main/java/com/example/engg1420/ui/MainApp.java
+++ src/main/java/com/example/engg1420/ui/MainApp.java
@@ -5,35 +5,22 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
-    private int c = 0;
-    private String r;
-    public void setC(int c){
-        this.c = c;
-    }
-    public int getC(){
-        return c;
-    }
+
 @Override
     public void start(Stage stage) throws Exception {
-        if(c == 0){
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/main.fxml"));
             Scene scene = new Scene(loader.load());
 
             stage.setScene(scene);
             stage.setTitle("Booking System Phase One");
             stage.show();
-        }
 
 }
 
 
 
 public static void main(String[] args){
-        /*if(c == 1){
-            launch();
-        }else{
-            System.out.println("test");
-        }*/
+
         launch();
     }
 }
