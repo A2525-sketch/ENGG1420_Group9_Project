595ad3125085ed425cb4e67410d9e1a497fb4a57
aelkeden <aelkeden@uoguelph.ca>
2026-03-02 03:47:34 -0500

    I have created the BookingManagementController and the bookingManagementApp and the .fxml file

---
+++ src/main/java/com/example/engg1420/ui/BookingManagementApp.java
@@ -0,0 +1,16 @@
+package com.example.engg1420.ui;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class BookingManagementApp extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/BookingManagement.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.setTitle("Booking Management");
+        stage.show();
+    }
+}
