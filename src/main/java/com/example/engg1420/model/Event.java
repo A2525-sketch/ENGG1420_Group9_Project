cf64da5c0892c1bf01bdf53450f5dbe021350db4
A2525-sketch <aadams16@uoguelph.ca>
2026-03-02 18:20:05 -0500

    changed multiple changes in EventManagerApp and EventManagerController as well as some changes in fxml files related to EventManager

--- src/main/java/com/example/engg1420/model/Event.java
+++ src/main/java/com/example/engg1420/model/Event.java
@@ -7,17 +7,16 @@ import java.util.List;
 
 public class Event {
     static private List<EventType> events = new ArrayList<>();
-    String input;
 
     //edit event
     public void setEvent(String eventid, String title, String date, String location, int capacity, String temp1, int temp2, int eventNum){
-        if(input == "Workshop"){
+        if(eventNum == 0){
             EventType e1 = new Workshop(eventid, title, date, location, capacity, temp1);
             events.add(0, e1);
-        }else if(input == "Seminar"){
+        }else if(eventNum == 1){
             EventType e2 = new Seminar(eventid, title, date, location, capacity, temp1);
             events.add(1, e2);
-        }else if(input == "Concert"){
+        }else if(eventNum == 2){
             EventType e3 = new Concert(eventid,title, date, location, capacity, temp2);
             events.add(2, e3);
         }
