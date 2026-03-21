b612c670c42fdf2bbff999b6669a516a75f60105
abdul <ahasan10@uoguelph.ca>
2026-03-21 11:53:28 -0400

    this was a test to see if the mongodb worked, right now there are some erros, still working on it

---
+++ src/main/java/TestMongo.java
@@ -0,0 +1,16 @@
+import com.mongodb.client.*;
+import org.bson.Document;
+
+public class TestMongo {
+    public static void main(String[] args) {
+        MongoClient client = MongoClients.create("mongodb://localhost:27017");
+        MongoDatabase db = client.getDatabase("testdb");
+        MongoCollection<Document> users = db.getCollection("users");
+
+        users.insertOne(new Document("name", "Hasan").append("age", 22));
+
+        for (Document doc : users.find()) {
+            System.out.println(doc.toJson());
+        }
+    }
+}
