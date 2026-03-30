2164c28d0a36c26e5d1448677c0dce07a294a7ce
idk-214 <lalr@uoguelph.ca>
2026-03-30 16:42:18 -0400

    database manager class

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,10 @@
 module com.example.engg1420 {
-
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens com.example.engg1420.ui to javafx.fxml;
+    requires org.mongodb.driver.sync.client;
+    requires org.mongodb.bson;
 
+    opens com.example.engg1420.ui to javafx.fxml;
     exports com.example.engg1420.ui;
 }
