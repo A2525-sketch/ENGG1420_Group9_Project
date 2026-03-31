4404dda83d29cd00dd817b6b3f5675353d178dd1
Vampire Byte <vampirebytestudios@gmail.com>
2026-03-31 00:01:46 -0400

    BOOKING work in progress (per user booking function)

--- src/main/java/com/example/engg1420/ui/BookingManagementController.java
+++ src/main/java/com/example/engg1420/ui/BookingManagementController.java
@@ -14,9 +14,14 @@ import java.util.List;
 public class BookingManagementController {
 
     @FXML
-    private ListView<String> bookinglist; // must match fx:id in FXML
+    private ListView<String> bookinglist;
+    @FXML
+    private ListView<String> mybookinglist;
     private List<User> users;
+    @FXML
+    private ListView<String> bookingList;
     private ObservableList<String> bkObservableList;
+
     @FXML
     private Label loginstatus;
 
@@ -75,10 +80,48 @@ public class BookingManagementController {
         if (!login) {
             loginstatus.setText("Login failed: no user with ID " + enteredId);
         }
+
     }
     @FXML
     private void handleBook() {
-        if(login == true){}
+        System.out.println("Book button clicked!"); // DEBUG
+        String selected = mybookinglist.getSelectionModel().getSelectedItem();
+        if (selected == null) return;
+
+        String[] parts = selected.split("\\|");
+        String userId = parts[0].trim();
+
+        // Create Booking object
+        Booking booking = new Booking(
+                "B" + System.currentTimeMillis(),
+                userId,
+                "E101", // later: from event selection
+                java.time.LocalDateTime.now().toString(),
+                com.example.engg1420.model.BookingStatus.CONFIRMED
+        );
+
+        // Save to user-specific CSV
+        saveBookingForUser writer = new saveBookingForUser();
+        writer.saveBookingForUser(booking);
+
+        System.out.println("Saved booking for user: " + userId);
+        //load user bookings
+        String filename = "bookings_user_" + userId + ".csv";
+
+        bookingList.getItems().clear();
+
+        try (java.io.BufferedReader reader =
+                     new java.io.BufferedReader(new java.io.FileReader(filename))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                bookingList.getItems().add(line);
+            }
+
+        } catch (Exception e) {
+            System.out.println("No bookings yet for this user.");
+        }
+
     }
     @FXML
     private void handleCancel() {
